e96c0170a85c6fab442858aa7190f028e5edf94e
Valentino Traverso <vt20032003@live.com.ar>
2023-06-07 16:27:34 +0200

    feat: first banner main

--- src/components/pages/homepage/PrincipalBanner/PrincipalBanner.tsx
+++ src/components/pages/homepage/PrincipalBanner/PrincipalBanner.tsx
@@ -1,9 +1,17 @@
+import Image from 'next/image';
 import styles from './PrincipalBanner.module.scss';
 
-export default function PrincipalBanner(){
-    return(
+export default function PrincipalBanner() {
+    return (
         <section className={styles["container-principal-banner"]}>
-
+            <div className={styles["container-grid-principal"]}>
+                <div>
+                    <span className={styles["first-text-main"]}>Bienvenido a la revolución de la fidelización.</span>
+                </div>
+                <div>
+                    <Image src="/assets/images/main/mobile-map-iphone.png" alt='image mobile' width={500} height={500} />
+                </div>
+            </div>
         </section>
     )
 }
