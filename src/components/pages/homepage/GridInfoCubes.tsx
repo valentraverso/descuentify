8dbf141664bfcb156c5b26b19a93b21ec6c9a140
Valentino Traverso <vt20032003@live.com.ar>
2023-06-07 10:02:13 +0200

    add: component gridinfocubes & set title

---
+++ src/components/pages/homepage/GridInfoCubes.tsx
@@ -0,0 +1,18 @@
+export default function Typeform() {
+    return (
+        <section>
+            <h2>¿Porque usar Descuentify?</h2>
+            <div>
+                <div>
+                    <p>¿Que es Descuentify?</p>
+                </div>
+                <div>
+                    <p>¿Para que sirve?</p>
+                </div>
+                <div>
+                    <p>¿Porque lo hacemos?</p>
+                </div>
+            </div>
+        </section>
+    )
+}
