f8da97ba592a1a724599a0761a5c8752dc10f4ff
Valentino Traverso <vt20032003@live.com.ar>
2023-06-19 19:25:41 +0200

    feat: last changes banner

--- src/components/pages/homepage/PrincipalBanner/PrincipalBanner.tsx
+++ src/components/pages/homepage/PrincipalBanner/PrincipalBanner.tsx
@@ -6,7 +6,8 @@ export default function PrincipalBanner() {
         <section className={styles["container-principal-banner"]}>
             <div className={styles["container-grid-principal"]}>
                 <div>
-                    <span className={styles["first-text-main"]}>Bienvenido a la revolución de la fidelización.</span>
+                    <span className={styles["first-text-main"]}>Bienvenido a la revolución de la <span className={styles.purple}>fidelización.</span></span>
+                    <span className={styles["principal-button-main"]}>Me quiero unir</span>
                 </div>
                 <div>
                     <Image src="/assets/images/main/mobile-map-iphone.png" alt='image mobile' width={500} height={500} />
