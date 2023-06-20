e3e40fad6689cd53214b55a4d583b6981fc22af0
zzezequiel <ezequielzvirgzdins@gmail.com>
2023-06-20 07:58:56 +0200

    add: frasse

--- src/components/pages/homepage/PrincipalBanner/PrincipalBanner.tsx
+++ src/components/pages/homepage/PrincipalBanner/PrincipalBanner.tsx
@@ -8,6 +8,11 @@ export default function PrincipalBanner() {
                 <div>
                     <span className={styles["first-text-main"]}>Bienvenido a la revolución de la <span className={styles.purple}>fidelización.</span></span>
                     <span className={styles["principal-button-main"]}>Me quiero unir</span>
+                    <div className={styles['container-text']}>
+                        <h2 className={styles.text}>
+                            Hacer que vuelvas 🫂
+                        </h2>
+                    </div>
                 </div>
                 <div>
                     <img className={styles['mobile-img']} src="/assets/img/main/mobile-map-iphone.png" alt='image mobile' />
