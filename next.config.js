10aa8e4e1cf8fe84aecefefb9e30b2c3d5ccc647
zzezequiel <ezequielzvirgzdins@gmail.com>
2023-06-22 06:22:26 +0200

    first commit

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['*', 'localhost', 'lh3.googleusercontent.com', 'thumbs.dreamstime.com', 'cdn.shopify.com', 'res.cloudinary.com', 'giphy.com'],
-      },
-}
-
-module.exports = nextConfig
--- src/components/pages/homepage/features/Features.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-interface Props {}
-
-const Features = () => {
-  return <div></div>
-}
-
-export default Features
